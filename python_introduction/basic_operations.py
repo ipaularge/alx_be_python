fd0224e656f38d7ca1f98d3eed8fba1a8c43e91c
paularge <monosourcepro@gmail.com>
2025-06-01 09:39:35 +0100

    add basic_operations.py script for arithmetic task

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+# Define variables
+number1 = 10
+number2 = 5
+
+# perform arithmetic operations
+sum_result = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# print results
+print("addition of", number1, "and", number2, "is", sum_result)
+print("subtraction of", number1, "and", number2, "is", difference)
+print("multiplication of", number1, "and", number2, "is", product)
+
